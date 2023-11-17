46b5d84dac0cb4df92292ed1761c112ea3309fa8
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-11-18 06:27:36 +0900

    Update .lua

--- .lua
+++ .lua
@@ -33,6 +33,18 @@ Icon = "rbxassetid://",
 PremiumOnly = false
 })
 
+local T4 = Window:MakeTab({
+Name = "Teleport",
+Icon = "rbxassetid://",
+PremiumOnly = false
+})
+
+local T5 = Window:MakeTab({
+Name = "BOSS",
+Icon = "rbxassetid://",
+PremiumOnly = false
+})
+
 local T3 = Window:MakeTab({
 Name = "HALLOWEEN",
 Icon = "rbxassetid://",
@@ -43,11 +55,13 @@ local egg = {}
 local boss = {}
 local dummies = {}
 local enemies = {}
+local zone = {}
 
 OrionLib:AddTable(workspace["_MAP"]["Eggs"],egg)
 OrionLib:AddTable(workspace["_MAP"]["Bosses"],boss)
 OrionLib:AddTable(workspace["_MAP"]["TrainDummy"],dummies)
-OrionLib:AddTable(Workspace["_THINGS"]["Coins"],enemies)
+OrionLib:AddTable(workspace["_THINGS"]["Coins"],enemies)
+OrionLib:AddTable(workspace["_MAP"]["Zones"],zone)
 
 T1:AddDropdown({
 Name = "Select Weapon",
@@ -55,6 +69,33 @@ Default = "Sword",
 Options = {"Sword","Barbell"},
 Callback = function(Value)
 game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["CoreService"]["RF"]["EquipTool"]:InvokeServer(Value)
+end    
+})
+
+T4:AddDropdown({
+Name = "Select Zone to teleport",
+Default = zone[1],
+Options = zone,
+Callback = function(Value)
+game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["TeleportService"]["RF"]["Teleport"]:InvokeServer(Value)
+end    
+})
+
+T5:AddDropdown({
+Name = "Select Boss",
+Default = boss[1],
+Options = boss,
+Callback = function(Value)
+_G.Boss_Atk = Value
+end    
+})
+
+T4:AddDropdown({
+Name = "Select Zone to buy",
+Default = zone[1],
+Options = zone,
+Callback = function(Value)
+game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["TeleportService"]["RF"]["BuyAreaAccess"]:InvokeServer(Value)
 end    
 })
 
@@ -135,6 +176,30 @@ _G.halloween = Value
       while wait() do
         if _G.halloween == false then break end
         game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["HalloweenService"]["RF"]["AttackBoss"]:InvokeServer(dum)
+      end
+end    
+})
+
+T5:AddToggle({
+Name = "Start Fight",
+Default = false,
+Callback = function(Value)
+_G.f = Value
+      while wait() do
+        if _G.f == false then break end
+        game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["BossesService"]["RF"]["FightBoss"]:InvokeServer(_G.Boss_Atk)
+      end
+end    
+})
+
+T5:AddToggle({
+Name = "Auto Attack",
+Default = false,
+Callback = function(Value)
+_G.b_atk = Value
+      while wait() do
+        if _G.b_atk == false then break end
+        game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["BossesService"]["RF"]["AttackBoss"]:InvokeServer()
       end
 end    
 })
