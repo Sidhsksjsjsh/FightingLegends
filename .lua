96adfd9517e0c56324eba9b471a906e4b6c21323
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-11-17 20:55:57 +0900

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,78 @@
+--[[
+ More Feature Coming Soon!
+]]
+
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/NMEHkVTb"))()
+local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3", HidePremium = false, SaveConfig = false, ConfigFolder = "TurtleFi"})
+local workspace = game:GetService("Workspace")
+
+local T1 = Window:MakeTab({
+Name = "Main",
+Icon = "rbxassetid://",
+PremiumOnly = false
+})
+
+local T2 = Window:MakeTab({
+Name = "Egg",
+Icon = "rbxassetid://",
+PremiumOnly = false
+})
+
+local egg = {}
+local boss = {}
+OrionLib:AddTable(workspace._MAP.Eggs,shit)
+OrionLib:AddTable(workspace._MAP.Bosses,boss)
+
+T1:AddDropdown({
+Name = "Select Weapon",
+Default = "Sword",
+Options = {"Sword","Barbell"},
+Callback = function(Value)
+game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["CoreService"]["RF"]["EquipTool"]:InvokeServer(Value)
+end    
+})
+
+T2:AddDropdown({
+Name = "Select Egg",
+Default = egg[1],
+Options = egg,
+Callback = function(Value)
+_G.S_Egg = Value
+end    
+})
+
+T1:AddToggle({
+Name = "Click",
+Default = false,
+Callback = function(Value)
+_G.Click = Value
+      while wait() do
+        if _G.Click == false then break end
+        game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["CoreService"]["RF"]["Click"]:InvokeServer()
+      end
+end    
+})
+
+T1:AddToggle({
+Name = "Rebirth",
+Default = false,
+Callback = function(Value)
+_G.Rb = Value
+      while wait() do
+        if _G.Rb == false then break end
+        game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["RebirthService"]["RF"]["Rebirth"]:InvokeServer()
+      end
+end    
+})
+
+T2:AddToggle({
+Name = "Hatch",
+Default = false,
+Callback = function(Value)
+_G.Htc = Value
+      while wait() do
+        if _G.Htc == false then break end
+        game:GetService("ReplicatedStorage")["Framework"]["Packages"]["Knit"]["Services"]["_HatchService"]["RF"]["BuyEgg"]:InvokeServer(_G.S_Egg,1)
+      end
+end    
+})
